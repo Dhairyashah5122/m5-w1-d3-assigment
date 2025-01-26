e3ad7661f3bc1f942d5e1f13bce3ef2bb12d84cf
dhairya <d.shah.314@westcliff.edu>
2025-01-25 17:02:42 -0700

    commit#3

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const CheckOut = () => (
   <div className="checkout">
-    <h2>Check Out</h2>
+    <h2>Check Out page</h2>
     <p>This is the Check Out screen after Facebook login.</p>
   </div>
 );
